1d436844ae2d4a55163c89f46b81bc5f31a9b85d
Trigalo27 <internick27@gmail.com>
2021-02-23 10:33:43 -0600

    comit goty

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: 'http://localhost:5000/firestore-grafica2/us-central1',
+  firebase: {
+    apiKey: "AIzaSyA4STnG9c8hdBZtnqmjXBDU5eYbWZSNB_I",
+    authDomain: "firestore-grafica2.firebaseapp.com",
+    databaseURL: "https://firestore-grafica2.firebaseio.com",
+    projectId: "firestore-grafica2",
+    storageBucket: "firestore-grafica2.appspot.com",
+    messagingSenderId: "282007981169",
+    appId: "1:282007981169:web:f9f631bb2bdf79e6391fd0",
+    measurementId: "G-21NSTGKJSL"
+  }
 };
 
 /*
